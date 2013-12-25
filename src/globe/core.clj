cf536b4bc6f6ee21da0fa494f603d7874a41d451
Eric Fode <ericfode@gmail.com>
2013-12-25 02:31:27 -0800

    finished up basic world actions

--- src/globe/core.clj
+++ src/globe/core.clj
@@ -3,56 +3,110 @@
    [clojure.core.async :refer [chan]]
    [clojure.core.typed :refer :all]))
 
-(ann-record World [event-queue :- Chan
-                   world-map   :- Map
-                   tic         :- AnyInteger])
-(defrecord World [event-queue world-map tic])
+;Types!!
 
-(ann-record Location [world :- Symbol
-                      level :- Symbol
-                      x     :- AnyInteger
-                      y     :- AnyInteger])
-(defrecord Location [world level x y])
 
-(ann loc-0 Location)
-(def loc-0 (Location. ':world ':1 0 0))
+(def-alias World-Map Map)
 
+(def-alias World '{:event-queue  Chan
+                   :world-map    World-Map
+                   :tic          AnyInteger})
+
+(def-alias World-Path  '[Symbol Symbol])
+(def-alias Point       '[AnyInteger AnyInteger])
+(def-alias Location    '[World-Path Point])
+
+(def-alias Spot (U World-Path
+                   Location))
+
+(def-alias Square (List Obj))
+(def-alias Grid (Vec (Vec Square)))
+
+(def-alias Obj Any)
+(def-alias Tic AnyInteger)
+
+(def-alias Node-Params '{:obj Obj :location Location})
+(def-alias Add-Node '[:add-node Node-Params])
+(def-alias Remove-Node '[:remove-node Node-Params])
 
 (def-alias Action
-  (U (I ':add-node Any Location)
-     (I ':remove-node Any Location)))
+  (U Add-Node
+     Remove-Node))
+
+(def-alias Event  '{:sender Obj :action Action :tic Tic})
 
 
-(ann-record Event [doer      :- Any
-                   action    :- Action
-                   tic       :- Anyinteger])
-(defrecord Event [doer action tic])
+(ann loc-0 Location)
+(def loc-0 [:awesome-world :level-1 0 0])
 
-
-(cf ('add-node 1 loc-0) Action)
-
+(ann  world [Chan Map AnyInteger -> World])
+(defn world [ch m x] {:event-queue ch :world-map m :tic x})
 
 (ann empty-world World)
-(def empty-world
-  (World. (chan) {} 0))
+(def empty-world (world chan
+                  {:awesome-world {:level-1 [['(1 7) '(2 3)]
+                                             []
+                                             []
+                                             []]}}
+                  0))
 
-(ann  get-location [World Location -> (Option (Vec Any))])
-(defn get-location
+
+(ann  get-spot [World Spot -> (Option (U Square Grid))])
+(defn get-spot
   "Givein a location get-location navigates the world
   to try to find the place asked for, it will return a
   list of things found"
-  [world {:keys [world level x y]}]
-  (world (level (get x (get y world)))))
+  [world spot]
+  (get-in (:world-map world) spot))
+
+(ann update-spot [World Spot (U Square Grid) -> (Option (U Grid World-Map))])
+
+(defn update-spot
+  [world spot o]
+  (update-in (:world-map world) spot #(o)))
+
+(ann  assoc-spot [World Spot (U Square Grid) -> (Option (U Grid World-Map))])
+(defn assoc-spot
+  [world loc o]
+  (assoc-in (:world-map world) loc o))
 
 (ann tic [World -> World])
 (defn tic [world]
   (assoc world :tic (inc (:tic world))))
 
-(ann run-event [World Event -> World])
-(defmulti run-event (fn [world event] (:action event)))
+(defn loc-wp
+  [[w l _ _]] (vec [w l]))
 
-(defmethod run-event :add-node [world {:keys [doer action paramaters tic]}]
-  (get-location world (:location paramaters)))
+
+(def tag first)
+(def body rest)
+(ann run-event [World Event -> World])
+(defmulti run-event (fn [_ event] tag (:action event)))
+
+(defmethod run-event :add-node [world {[_ action] :action}]
+  (let [loc (:location action)
+        square (get-spot world loc)
+        obj    (:obj action)
+        updated-square (conj square obj)]
+  (assoc-spot world loc updated-square)))
+
+(defmethod run-event :remove-node [world {[_ action] :action}]
+  (let [loc (:location action)
+        square (get-spot world loc)
+        obj    (:obj action)
+        updated-square (remove (partial = obj) square )]
+  (assoc-spot world loc updated-square)))
+
+(ann test-event Event)
+(def test-event {:sender 0 :action [:remove-node {:obj 1 :location loc-0}] :tic 0 })
+
+(run-event empty-world test-event)
+
+(get-location empty-world loc-0)
+
+
+
+
 
 
 
