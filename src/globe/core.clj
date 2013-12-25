d2759b8b40929a3a85e6845cc4e425a28b58b039
Eric Fode <ericfode@gmail.com>
2013-12-24 18:09:33 -0800

    really really ugly starting place

--- src/globe/core.clj
+++ src/globe/core.clj
@@ -1,6 +1,60 @@
-(ns globe.core)
+(ns globe.core
+  (:require
+   [clojure.core.async :refer [chan]]
+   [clojure.core.typed :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ann-record World [event-queue :- Chan
+                   world-map   :- Map
+                   tic         :- AnyInteger])
+(defrecord World [event-queue world-map tic])
+
+(ann-record Location [world :- Symbol
+                      level :- Symbol
+                      x     :- AnyInteger
+                      y     :- AnyInteger])
+(defrecord Location [world level x y])
+
+(ann loc-0 Location)
+(def loc-0 (Location. ':world ':1 0 0))
+
+
+(def-alias Action
+  (U (I ':add-node Any Location)
+     (I ':remove-node Any Location)))
+
+
+(ann-record Event [doer      :- Any
+                   action    :- Action
+                   tic       :- Anyinteger])
+(defrecord Event [doer action tic])
+
+
+(cf ('add-node 1 loc-0) Action)
+
+
+(ann empty-world World)
+(def empty-world
+  (World. (chan) {} 0))
+
+(ann  get-location [World Location -> (Option (Vec Any))])
+(defn get-location
+  "Givein a location get-location navigates the world
+  to try to find the place asked for, it will return a
+  list of things found"
+  [world {:keys [world level x y]}]
+  (world (level (get x (get y world)))))
+
+(ann tic [World -> World])
+(defn tic [world]
+  (assoc world :tic (inc (:tic world))))
+
+(ann run-event [World Event -> World])
+(defmulti run-event (fn [world event] (:action event)))
+
+(defmethod run-event :add-node [world {:keys [doer action paramaters tic]}]
+  (get-location world (:location paramaters)))
+
+
+
+
+
